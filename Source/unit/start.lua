8be67681f0ccba5f65ecdad3570afd1de062fc15
Thomas <jericho1060@gmail.com>
2021-07-11 13:29:07 +0200

    credits

--- Source/unit/start.lua
+++ Source/unit/start.lua
@@ -1,3 +1,9 @@
+--[[
+    DU-Nested-Coroutines by Jericho
+    Permit to easier avoid CPU Load Errors
+    Source available here: https://github.com/Jericho1060/du-nested-coroutines
+]]--
+
 coroutinesTable  = {}
 --all functions here will become a coroutine
 MyCoroutines = {
--- Source/update/update.lua
+++ Source/update/update.lua
@@ -1,3 +1,9 @@
+--[[
+    DU-Nested-Coroutines by Jericho
+    Permit to easier avoid CPU Load Errors
+    Source available here: https://github.com/Jericho1060/du-nested-coroutines
+]]--
+
 if coroutine.status(MainCoroutine) == "dead" then
   MainCoroutine = coroutine.create(runCoroutines)
 end
